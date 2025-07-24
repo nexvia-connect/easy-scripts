ff00a4212f26570fdc7d0e02f688319dc8891fef
nexvia-connect <xavier.bertrandias@nexvia.lu>
2025-07-24 13:04:00 +0200

    Update easy-suite.user.js

--- easy-suite.user.js
+++ easy-suite.user.js
@@ -1,17 +1,26 @@
 // ==UserScript==
-// @name         Easy Master Loader
-// @namespace    https://github.com/nexvia-connect/easy-scripts
+// @name         Easy Suite Loader
+// @namespace    https://github.com/yourusername/easy-scripts
 // @version      1.0
-// @description  Master loader that pulls in all Easy scripts
+// @description  Loads all Easy-* scripts from GitHub repo
 // @match        https://nexvia1832.easy-serveur53.com/*
-// @match        *://*/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ref-insert.user.js
-// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ui-cleaner.user.js
-// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-photo-resizer.user.js
-// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-full-width-description.user.js
 // ==/UserScript==
 
 (function () {
-  // Nothing here. Everything is loaded via @require.
+  'use strict';
+
+  const scripts = [
+    'https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ref-insert.user.js',
+    'https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ui-cleaner.user.js',
+    'https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-photo-resizer.user.js',
+    'https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-full-width-description.user.js'
+  ];
+
+  for (const url of scripts) {
+    const script = document.createElement('script');
+    script.src = url;
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+  }
 })();
