6f0922b4404ebe2459d46acbc638e7ec21e1e6af
nexvia-connect <xavier.bertrandias@nexvia.lu>
2025-07-24 13:03:16 +0200

    Create  easy-suite.user.js

---
+++ easy-suite.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Easy Master Loader
+// @namespace    https://github.com/nexvia-connect/easy-scripts
+// @version      1.0
+// @description  Master loader that pulls in all Easy scripts
+// @match        https://nexvia1832.easy-serveur53.com/*
+// @match        *://*/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ref-insert.user.js
+// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-ui-cleaner.user.js
+// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-photo-resizer.user.js
+// @require      https://raw.githubusercontent.com/yourusername/easy-scripts/main/easy-full-width-description.user.js
+// ==/UserScript==
+
+(function () {
+  // Nothing here. Everything is loaded via @require.
+})();
