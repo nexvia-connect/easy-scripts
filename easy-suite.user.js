67c3c2fea49b4c0116cee71eabd32f9bc90f0840
nexvia-connect <xavier.bertrandias@nexvia.lu>
2025-07-24 13:23:37 +0200

    Update easy-suite.user.js

--- easy-suite.user.js
+++ easy-suite.user.js
@@ -1,26 +1,16 @@
 // ==UserScript==
-// @name         Easy Suite
+// @name         Easy Suite Loader
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Loads all Easy scripts from GitHub
+// @description  Loads all Easy scripts in one
 // @match        https://nexvia1832.easy-serveur53.com/*
 // @grant        none
+// @require      https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-ref-insert.user.js
+// @require      https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-ui-cleaner.user.js
+// @require      https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-photo-resizer.user.js
+// @require      https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-full-width-description.user.js
 // ==/UserScript==
 
 (function () {
-  'use strict';
-
-  const scripts = [
-    'https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-ref-insert.user.js',
-    'https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-ui-cleaner.user.js',
-    'https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-photo-resizer.user.js',
-    'https://raw.githubusercontent.com/nexvia-connect/easy-scripts/main/easy-full-width-description.user.js'
-  ];
-
-  scripts.forEach(src => {
-    const s = document.createElement('script');
-    s.src = src + '?v=' + Date.now(); // prevent caching
-    s.type = 'text/javascript';
-    document.head.appendChild(s);
-  });
+  
 })();
